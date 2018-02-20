6e9fa4e1dbce8ffff34aa3eddf4551c6022079aa
WifekRaissi <wifek.raissi@esprit.tn>
2018-02-20 20:35:44 +0100

    v4

--- src/main/kotlin/com/kotlin/hellokotlinspring/HelloController.kt
+++ src/main/kotlin/com/kotlin/hellokotlinspring/HelloController.kt
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
-class UserController{
+class HelloController {
 
     val counter = AtomicLong()
 
     @GetMapping("/hello")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            User(counter.incrementAndGet(), "Hello, $name")
+             "Hello, $name"
 
 }
--- src/main/kotlin/com/kotlin/hellokotlinspring/User.kt
+++
@@ -1,5 +0,0 @@
-package com.kotlin.hellokotlinspring
-
-
-
-data class User(val id:Long, val name: String)
