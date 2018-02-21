7e59b183d93098312cd40c16da91e5d84d1ca8b3
WifekRaissi <wifek.raissi@esprit.tn>
2018-02-21 16:43:43 +0100

    v4

--- src/main/kotlin/com/kotlin/hellokotlinspring/HelloController.kt
+++ src/main/kotlin/com/kotlin/hellokotlinspring/HelloController.kt
@@ -3,7 +3,7 @@ package com.kotlin.hellokotlinspring
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.atomic.AtomicLong
+
 
 @RestController
 class HelloController {
